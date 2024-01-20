5923793875184823ae97e15317572a7ca9abf403
TruePartner312 <truepartner312@gmail.com>
2024-01-20 15:16:44 +0900

    update test.

--- src/main/index.ts
+++ src/main/index.ts
@@ -173,7 +173,7 @@ function createTray(): void {
   tray.setContextMenu(contextMenu)
 }
 
-// Updatedssssss.
+// Updated.
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
