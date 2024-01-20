8307113ec0c906fea97284f967b835563a85429d
TruePartner312 <truepartner312@gmail.com>
2024-01-20 14:48:03 +0900

    trr

--- src/main/index.ts
+++ src/main/index.ts
@@ -173,7 +173,7 @@ function createTray(): void {
   tray.setContextMenu(contextMenu)
 }
 
-// Updatedssss.
+// Updatedsssss.
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
